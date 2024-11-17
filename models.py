daa45f28d1a519d8f6d26b7e587ddd802078bd5e
al1sa-dream <zhandossaidana@gmail.com>
2024-11-18 01:43:10 +0600

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
 
-DATABASE_URL = "sqlite:///recipes.db"  # Убедитесь, что путь к базе данных верен
+DATABASE_URL = "sqlite:///recipes.db" 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 
 Base = declarative_base()
