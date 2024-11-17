619ecf5b11cefcf0a837d9f93e8f43defb94f42f
al1sa-dream <zhandossaidana@gmail.com>
2024-11-18 01:14:19 +0600

    Update api.py

--- api.py
+++ api.py
@@ -7,7 +7,6 @@ from werkzeug.utils import secure_filename
 
 app = FastAPI()
 
-# Проверяем, что папка для загрузок существует, и создаем ее, если нужно
 UPLOAD_FOLDER = "static/uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -21,14 +20,12 @@ class CommentModel(BaseModel):
 async def upload_comment(content: str = Form(...), recipe_id: int = Form(...),
                          user_id: int = Form(...), file: UploadFile = None,
                          db: Session = Depends(get_db)):
-    # Временный фиксированный user_id для отладки
-    user_id = 1  # Замените 1 на фактический ID пользователя в базе данных
+    user_id = 1 
 
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
         raise HTTPException(status_code=400, detail="User not found")
 
-    # Сохраняем файл, если он предоставлен
     image_path = None
     if file:
         filename = secure_filename(file.filename)
@@ -36,7 +33,6 @@ async def upload_comment(content: str = Form(...), recipe_id: int = Form(...),
         with open(image_path, "wb") as image_file:
             image_file.write(await file.read())
 
-    # Создаем комментарий и сохраняем его в базе данных
     comment = Comment(content=content, user_id=user_id, recipe_id=recipe_id, image_path=image_path)
     db.add(comment)
     db.commit()
